c91154c1af0fe1c9be33600fb488cd491b7e77fd
Pablo Heidenreich <heidenreich20@hotmail.com>
2023-10-25 00:59:48 -0300

    minor fixes

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,7 +1,7 @@
-export default function LogoutButton() {
+export default function LogoutButton () {
   return (
-    <form action="/auth/sign-out" method="post">
-      <button className="py-2 px-4 rounded-md no-underline bg-red-500 hover:bg-btn-background-hover">
+    <form action='/auth/sign-out' method='post'>
+      <button className='py-2 px-4 rounded-md no-underline bg-red-500 hover:bg-btn-background-hover'>
         Logout
       </button>
     </form>
