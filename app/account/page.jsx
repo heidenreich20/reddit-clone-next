e6d2fc3ed86620580798b5fabe93695344142c0f
Pablo Heidenreich <heidenreich20@hotmail.com>
2023-10-25 01:41:21 -0300

    minor fixes

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -2,12 +2,12 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import AccountForm from './account-form'
 
-export default async function Account() {
+export default async function Account () {
   const cookieStore = cookies()
   const supabase = createServerComponentClient({ cookies: () => cookieStore })
 
   const {
-    data: { session },
+    data: { session }
   } = await supabase.auth.getSession()
 
   return <AccountForm session={session} />
