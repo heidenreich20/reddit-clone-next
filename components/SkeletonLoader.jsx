99db264632d44cd21febb4d18fb159adc5eb8ed2
Pablo Heidenreich <heidenreich20@hotmail.com>
2023-10-25 01:34:40 -0300

    minor fixees

--- components/SkeletonLoader.jsx
+++ components/SkeletonLoader.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SkeletonLoader = ({ width, height }) => {
+const SkeletonLoader = ({ width }) => {
   if (width === 'image') {
     return (
       <div className='bg-neutral-300/[0.7] w-fit mx-auto aspect-square h-36 p-2 rounded-full text-transparent animate-pulse'>Loading...</div>
