2db1c70cd0379ca17dae0bba0db4ecb0fa2cc36a
Pablo Heidenreich <44761758+heidenreich20@users.noreply.github.com>
2023-12-05 17:50:24 -0300

    Update page.jsx

--- app/c/[community]/[posts]/page.jsx
+++ app/c/[community]/[posts]/page.jsx
@@ -2,8 +2,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import PostBody from './post-body'
 
-export default async function () {
-
+export default async function profile () {
   const cookieStore = cookies()
   const supabase = createServerComponentClient({ cookies: () => cookieStore })
 
@@ -11,7 +10,6 @@ export default async function () {
     data: { session }
   } = await supabase.auth.getSession()
 
-  return (
-    <PostBody session={session} />
-  )
+  return <PostBody session={session} />
+}
 
